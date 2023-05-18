84a97df62488874756c436b8b912ea097ecf7523
DylanCaff <dylancaff0902@gmail.com>
2023-05-18 14:56:51 -0400

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+
 
 const SocialLinks = () => {
-  return (
-    <div>SocialLinks</div>
-  )
-}
+    return (
+        <div className='flex flex-col top-[35%] left-0 fixed'>
+            <ul>
+                <li className='felx justify-between items-center w-40 h-1/4 px-4 bg-black'>
+                    <a
+                        href=''
+                        className='felx justify-between items-center w-full text-white'
+                    >
+                        <>
+                            LinkedIn <FaLinkedin size={30} />
+                        </>
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+    );
+};
 
 export default SocialLinks
