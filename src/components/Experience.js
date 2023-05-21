4c45da20dcb3486648468c9a4460c4651cdb6f4c
DylanCaff <dylancaff0902@gmail.com>
2023-05-21 12:11:17 -0400

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -71,7 +71,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-emerald-800 via-black to-black w-full h-screen"
+      className="bg-gradient-to-b from-emerald-800 via-black to-black w-full h-full"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
